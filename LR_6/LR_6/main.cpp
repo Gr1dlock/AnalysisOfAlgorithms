265e10a4984284fde5a38b51261a4b31dd55f34a
Dmitriy Lugovoy <dmit.lugovoy@gmail.com>
2019-12-17 01:12:58 +0300

    Comparison was added

--- LR_6/LR_6/main.cpp
+++ LR_6/LR_6/main.cpp
@@ -1,34 +1,111 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <chrono>
+#include <fstream>
 
 #include "travellingsalesman.h"
+
+struct Parameters
+{
+    int t;
+    double alpha;
+    double rho;
+    double diff;
+};
+
+static std::vector<Parameters> comparison;
+
+void writeComparison(const std::string &name)
+{
+    std::ofstream file;
+    file.open(name, std::ios_base::app);
+    if (!file)
+    {
+        std::cout << "Can't open file" << std::endl;
+    }
+    else
+    {
+        for (const auto &elem: comparison)
+        {
+            file << elem.rho << "," << elem.alpha << "," << elem.t << "," << elem.diff << '\n';
+        }
+        std::cout << "Comparison was written successfully" << std::endl;
+        file.close();
+    }
+}
+
+void sortComparison()
+{
+    bool flag;
+    for (size_t i = 0; i < comparison.size(); i++)
+    {
+        flag = false;
+        for (size_t j = 1; j < comparison.size() - i; j ++)
+        {
+            if (comparison[j].diff < comparison[j - 1].diff)
+            {
+                Parameters tmp = comparison[j];
+                comparison[j] = comparison[j - 1];
+                comparison[j - 1] = tmp;
+                flag = true;
+            }
+        }
+        if (!flag) return;
+    }
+}
+
+void writeTime(const std::string &name, int &n, double &bruteTime, double &antTime)
+{
+    std::ofstream file;
+    file.open(name, std::ios_base::app);
+    if (!file)
+    {
+        std::cout << "Can't open file" << std::endl;
+    }
+    else
+    {
+        file << n << "," << bruteTime << "," << antTime << '\n';
+        file.close();
+    }
+}
 
 int main()
 {
     srand(time(nullptr));
-    Matrix<double> matrix = { { 0, 8, 7, 1, 5, 5, 4, 9, 4, 3 },
-                               { 8, 0, 8, 7, 8, 5, 9, 4, 2, 2 },
-                               { 7, 8, 0, 7, 7, 4, 6, 9, 2, 9 },
-                               { 1, 7, 7, 0, 2, 1, 2, 2, 10, 5 },
-                               { 5, 8, 7, 2, 0, 9, 1, 8, 3, 2 },
-                               { 5, 5, 4, 1, 9, 0, 9, 4, 1, 6 },
-                               { 4, 9, 6, 2, 1, 9, 0, 3, 3, 1 },
-                               { 9, 4, 9, 2, 8, 4, 3, 0, 6, 8 },
-                               { 4, 2, 2, 10, 3, 1, 3, 6, 0, 7 },
-                               { 3, 2, 9, 5, 2, 6, 1, 8, 7, 0 } };
+    Matrix<double> matrix = {
+    { 0, 4, 8, 6, 8, 10, 9, 5, 8, 6 },
+    { 4, 0, 3, 8, 7, 8, 10, 4, 9, 6 },
+    { 8, 3, 0, 3, 6, 3, 6, 10, 7, 9 } ,
+    { 6, 8, 3, 0, 2, 8, 5, 3, 9, 9 },
+    { 8, 7, 6, 2, 0, 2, 7, 6, 6, 7 },
+    { 10, 8, 3, 8, 2, 0, 6, 3, 10, 9 },
+    { 9, 10, 6, 5, 7, 6, 0, 1, 7, 6 },
+    { 5, 4, 10, 3, 6, 3, 1, 0, 4, 1 },
+    { 8, 9, 7, 9, 6, 10, 7, 4, 0, 9 },
+    { 6, 6, 9, 9, 7, 9, 6, 1, 9, 0 }};
     matrix.print();
-    Route result = ant(matrix, 100, 0.5, 0.5);
-    std::cout << "Ant result: " << result.length << " length" << std::endl;
-    for (const auto &element: result.path)
+    Route result = bruteForce(matrix);
+    double diff;
+    double ideal = result.length;
+    for (double rho = 0; rho <= 1; rho += 0.25)
     {
-        std::cout << element << " ";
+        for (double alpha = 0; alpha <= 1; alpha += 0.25)
+        {
+            for (int t = 10; t <= 310; t += 10)
+            {
+                diff = 0;
+                for (auto i = 0; i < 100; i++)
+                {
+                    result = ant(matrix, t, alpha, rho);
+                    diff += result.length - ideal;
+                }
+                diff /= 100.0;
+                comparison.push_back({t, alpha, rho, diff});
+            }
+        }
     }
-    std::cout << std::endl;
-    result = bruteForce(matrix);
-    std::cout << "Brute force result: " << result.length << " length" << std::endl;
-    for (const auto &element: result.path)
-    {
-        std::cout << element << " ";
-    }
-    std::cout << std::endl;
+    sortComparison();
+    writeComparison("difference.txt");
     return 0;
 }
